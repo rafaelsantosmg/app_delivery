a73e605df40a0ef093247f280427ee8fc39193fd
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-08-06 15:14:10 -0300

    Iniciando Projeto APP Delivery

--- scripts/reset-db-dev-debug.sh
+++
@@ -1,6 +0,0 @@
-export NODE_ENV=development
-cd ./back-end
-npx sequelize-cli db:drop --debug
-npx sequelize-cli db:create --debug
-npx sequelize-cli db:migrate --debug
-npx sequelize-cli db:seed:all --debug
--- scripts/reset-db-dev.sh
+++
@@ -1,6 +0,0 @@
-export NODE_ENV=development
-cd ./back-end
-npx sequelize-cli db:drop
-npx sequelize-cli db:create
-npx sequelize-cli db:migrate
-npx sequelize-cli db:seed:all
