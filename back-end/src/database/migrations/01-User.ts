f9aa1372f00be1e5e6106eb5b8b41a2f0015cb6d
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-08-06 22:00:18 -0300

    chore(Migration): Criado as migration para criação das tebelas do banco de dados

---
+++ back-end/src/database/migrations/01-User.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class User1659826561658 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "role",
+            type: "varchar",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
---
+++ back-end/src/database/migrations/04-Product.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Product1659826704763 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "products",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar(100)",
+          },
+          {
+            name: "price",
+            type: "decimal(4,2)",
+          },
+          {
+            name: "url_image",
+            type: "varchar(200)",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("products");
+  }
+}
