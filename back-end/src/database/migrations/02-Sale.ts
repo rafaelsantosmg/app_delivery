7a734c53a649678a8b8cbfa621d06fd6ba131ad0
eemr3 <eemr3@yahoo.com.br>
2022-08-07 23:28:08 -0300

    refactor(migrations): refatora forma de cria id incrementavel

--- back-end/src/database/migrations/02-Sale.ts
+++ back-end/src/database/migrations/02-Sale.ts
@@ -1,63 +1,64 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Sale1659826696388 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "sales",
+        name: 'sales',
         columns: [
           {
-            name: "id",
-            type: "int",
+            name: 'id',
+            type: 'int',
             isPrimary: true,
-            generationStrategy: "increment",
+            isGenerated: true,
+            generationStrategy: 'increment',
           },
           {
-            name: "user_id",
-            type: "int",
+            name: 'user_id',
+            type: 'int',
           },
           {
-            name: "seller_id",
-            type: "int",
+            name: 'seller_id',
+            type: 'int',
           },
           {
-            name: "total_price",
-            type: "decimal(9,2)",
+            name: 'total_price',
+            type: 'decimal(9,2)',
           },
           {
-            name: "delivery_address",
-            type: "varchar(100)",
+            name: 'delivery_address',
+            type: 'varchar(100)',
           },
           {
-            name: "delivery_number",
-            type: "varchar(50)",
+            name: 'delivery_number',
+            type: 'varchar(50)',
           },
           {
-            name: "sale_date",
-            type: "date",
-            default: "now()",
+            name: 'sale_date',
+            type: 'date',
+            default: 'now()',
           },
           {
-            name: "status",
-            type: "varchar(50)",
+            name: 'status',
+            type: 'varchar(50)',
           },
         ],
         foreignKeys: [
           {
-            name: "fk_user",
-            columnNames: ["user_id"],
-            referencedTableName: "users",
-            referencedColumnNames: ["id"],
-            onDelete: "CASCADE",
-            onUpdate: "CASCADE",
+            name: 'fk_user',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
           },
           {
-            name: "fk_seller",
-            columnNames: ["seller_id"],
-            referencedTableName: "users",
-            referencedColumnNames: ["id"],
-            onDelete: "CASCADE",
-            onUpdate: "CASCADE",
+            name: 'fk_seller',
+            columnNames: ['seller_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
           },
         ],
       })
@@ -65,6 +66,6 @@ export class Sale1659826696388 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("sales");
+    await queryRunner.dropTable('sales');
   }
 }
