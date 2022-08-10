461011298b9c1c0ac2bcc12e4fa559ad935310e8
eemr3 <eemr3@yahoo.com.br>
2022-08-09 22:49:44 -0300

    feat(Register.service.ts): adiciona ao metodo createRegister as funções de criar token e criptografar a senha

--- back-end/src/services/Register.service.ts
+++ back-end/src/services/Register.service.ts
@@ -1,3 +1,5 @@
+import { hash } from 'bcryptjs';
+import Token from '../auth/Token';
 import Register from '../database/entities/Register';
 import { registerRepository } from '../database/repositories/Register.repository';
 import {
@@ -5,19 +7,37 @@ import {
   UserRequest,
 } from './interfaces/Register.service.interface';
 
+interface INewRegister extends Register {
+  token: string;
+}
 export default class RegisterService implements IRegister {
-  async createRegister(body: UserRequest): Promise<Register> {
-    const { email } = body;
+  private _createToken = new Token();
 
-    if (await registerRepository.findOne({ where: { email } })) {
+  async createRegister(body: UserRequest): Promise<INewRegister> {
+    if (await registerRepository.findOne({ where: { email: body.email } })) {
       throw new Error('User already exists');
     }
 
-    const register = registerRepository.create(body);
+    const pwdHash = await hash(body.password, 10);
 
+    const register = registerRepository.create({
+      name: body.name,
+      email: body.email,
+      password: pwdHash,
+      role: body.role,
+    });
+
+    const { id, name, email, role, password, sales } = register;
+
+    const token = this._createToken.createToken({
+      id,
+      name,
+      email,
+      role,
+    });
     await registerRepository.save(register);
 
-    return register;
+    return { id, name, email, role, password, sales, token };
   }
 
   async findAllRegisters(): Promise<Register[]> {
